06570cf3bc4e96284a584bb9b324d4169d54f1e5
YORDI <yordiazaelmzmzgmail.com>
2023-11-21 01:58:37 -0700

    Se realizó la creación de fragmentos, strings con traducciones, splash, logo, colores, temas y la navegación (faltó lógica, el diseño no está terminado, por lo que aún falta agregar varios colores, temas, etc.).

---
+++ app/src/main/java/com/yamz/administradordecontraseas/FirstFragment.kt
@@ -0,0 +1,32 @@
+package com.yamz.administradordecontraseas
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+
+class FirstFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val myView = inflater.inflate(R.layout.fragment_first, container, false)
+
+        val button = myView.findViewById<Button>(R.id.add_website)
+
+        button.setOnClickListener() {
+
+            val controller: NavController = Navigation.findNavController(myView)
+
+            controller.navigate(R.id.action_firstFragment_to_secondFragment)
+        }
+
+        return myView
+    }
+}
--- app/src/main/java/com/yamz/administradordecontraseas/MainActivity.kt
+++ app/src/main/java/com/yamz/administradordecontraseas/MainActivity.kt
@@ -2,10 +2,16 @@ package com.yamz.administradordecontraseas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        setTheme(R.style.Theme_AdministradorDeContraseñas)
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
