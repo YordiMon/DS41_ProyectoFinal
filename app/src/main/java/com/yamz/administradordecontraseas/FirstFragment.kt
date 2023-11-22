4222fdd22de4d4faaa1fb42e269370d3c162e237
YORDI <yordiazaelmzmzgmail.com>
2023-11-22 01:00:52 -0700

    Se creó el diseño de la ventana de login, se hicieron modificaciones a la ventana principa (modificaciones al diseño y nuevo boton para ir al fragmento de generar contraseña), se creó el diseño de la ventana para agregar una nueva nota (incompleto), se agregaron nuevos temas para los botones, se actualizó la plantilla de error, se agregó el fragmento vacío para ir a la ventana de generar contraseña

--- app/src/main/java/com/yamz/administradordecontraseas/FirstFragment.kt
+++ app/src/main/java/com/yamz/administradordecontraseas/FirstFragment.kt
@@ -1,13 +1,17 @@
 package com.yamz.administradordecontraseas
 
+import android.graphics.Typeface
 import android.os.Bundle
+import android.text.InputType
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class FirstFragment : Fragment() {
 
@@ -18,13 +22,33 @@ class FirstFragment : Fragment() {
 
         val myView = inflater.inflate(R.layout.fragment_first, container, false)
 
-        val button = myView.findViewById<Button>(R.id.add_website)
+        val enter = myView.findViewById<Button>(R.id.enter)
+        val cancel = myView.findViewById<Button>(R.id.cancel)
+        val user = myView.findViewById<EditText>(R.id.user)
+        val password = myView.findViewById<EditText>(R.id.password)
+        password.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+        password.setTypeface(Typeface.DEFAULT)
 
-        button.setOnClickListener() {
+        enter.setOnClickListener() {
 
-            val controller: NavController = Navigation.findNavController(myView)
+            if (user.text.isNotEmpty() and password.text.isNotEmpty()) {
 
-            controller.navigate(R.id.action_firstFragment_to_secondFragment)
+                val controller: NavController = Navigation.findNavController(myView)
+                controller.navigate(R.id.action_firstFragment_to_secondFragment)
+
+
+            } else {
+
+                val error = BottomSheetDialog(myView.context)
+                val layout: View = layoutInflater.inflate(R.layout.error_template, null)
+
+                error.setContentView(layout)
+                error.show()
+            }
+        }
+
+        cancel.setOnClickListener() {
+            requireActivity().finish()
         }
 
         return myView
--- app/src/main/java/com/yamz/administradordecontraseas/SecondFragment.kt
+++ app/src/main/java/com/yamz/administradordecontraseas/SecondFragment.kt
@@ -1,59 +1,57 @@
 package com.yamz.administradordecontraseas
 
 import android.os.Bundle
+import android.view.KeyEvent
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [SecondFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SecondFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false)
+
+        val myView = inflater.inflate(R.layout.fragment_second, container, false)
+
+        val add = myView.findViewById<Button>(R.id.add_website)
+        val gen = myView.findViewById<Button>(R.id.gen_password)
+
+        add.setOnClickListener() {
+
+            val controller: NavController = Navigation.findNavController(myView)
+            controller.navigate(R.id.action_secondFragment_to_thirdFragment)
+        }
+
+        gen.setOnClickListener() {
+
+            val controller: NavController = Navigation.findNavController(myView)
+            controller.navigate(R.id.action_secondFragment_to_fourthFragment)
+        }
+
+        return myView
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment secondFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SecondFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Asigna el listener al botón de retroceso
+        view.isFocusableInTouchMode = true
+        view.requestFocus()
+        view.setOnKeyListener { _, keyCode, event ->
+            if (keyCode == KeyEvent.KEYCODE_BACK && event.action == KeyEvent.ACTION_UP) {
+                // Cierra la aplicación completamente cuando se detecta el botón de retroceso en el fragmento
+                requireActivity().finishAffinity()
+                return@setOnKeyListener true
             }
+            false
+        }
     }
 }
